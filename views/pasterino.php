e2ee21b486d25d71b07030c044f1ec16decf9d08
CaptSiro <honzahorak433@icloud.com>
2023-08-06 15:24:55 +0200

    Created script to get session cookie

--- views/pasterino.php
+++ views/pasterino.php
@@ -10,7 +10,8 @@
     const PROJECT_PATH = "<?= $GLOBALS["project_path"] ?>";
   </script>
   <script src="<?= $GLOBALS["project_path"] ?>/public/js/pasterino.js" type="module" defer></script>
-  
+  <script src="<?= $GLOBALS["project_path"] ?>/public/js/session-cookie.js" type="module" defer></script>
+
   <title>Pasterino</title>
 </head>
 <body>
